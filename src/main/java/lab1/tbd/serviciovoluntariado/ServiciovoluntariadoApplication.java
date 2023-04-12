facef78b9763d0eea0f87dcda487dbb3c91d2dcf
kogekii <andres.ijurra@usach.cl>
2023-04-11 21:35:52 -0400

    [Andres Ijurra][feat] se crea el proyecto

---
+++ src/main/java/lab1/tbd/serviciovoluntariado/ServiciovoluntariadoApplication.java
@@ -0,0 +1,14 @@
+package lab1.tbd.serviciovoluntariado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiciovoluntariadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiciovoluntariadoApplication.class, args);
+		System.out.println("hello world");
+	}
+
+}
---
+++ src/test/java/lab1/tbd/serviciovoluntariado/ServiciovoluntariadoApplicationTests.java
@@ -0,0 +1,13 @@
+package lab1.tbd.serviciovoluntariado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiciovoluntariadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
