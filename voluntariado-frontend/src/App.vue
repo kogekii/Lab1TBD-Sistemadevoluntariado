0f69ef70ec3414776c74fcd9ea001afd9ab93e7f
kogekii <andres.ijurra@usach.cl>
2023-04-18 00:04:44 -0400

    [feat] se empieza el desarrollo del frontend

---
+++ voluntariado-frontend/src/App.vue
@@ -0,0 +1,41 @@
+
+
+<template>
+  <div :class="$estilo.container">
+    <div :class="$estilo.login">
+      <div :class="$estilo.loginrow1">
+        <h1>Login</h1>
+      </div>
+      <div :class="$estilo.loginrow2">
+        <label>Username</label>
+        <input />
+        <label>Password</label>
+        <input type="password"/>
+      </div>
+      <div :class="$estilo.loginrow3">
+        <button>enviar</button>
+        <a>registrarse</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+
+
+<script>
+import estilo from './App.module.css';
+export default {
+  data() {
+    return {
+      $estilo: estilo,
+    }
+  }
+}
+</script>
+
+<style module>
+body {
+  margin: 0;
+}
+</style>
