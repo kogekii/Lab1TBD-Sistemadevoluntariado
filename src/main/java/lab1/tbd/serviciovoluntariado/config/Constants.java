d7bcb7727e43e2be3399981b2807baa863603a51
Esteban Glochon <esteban.glochon@usach.cl>
2023-06-08 18:21:54 -0400

    [Esteban Glochon] seeder para database.

---
+++ src/main/java/lab1/tbd/serviciovoluntariado/config/Constants.java
@@ -0,0 +1,8 @@
+package lab1.tbd.serviciovoluntariado.config;
+
+public class Constants {
+    /* Datos por defecto para usuario admin */
+    public static final String ADMIN_NAME = "Admin Spring";
+    public static final String ADMIN_EMAIL = "admin@example.com";
+    public static final String ADMIN_PASSWORD = "password";
+}
